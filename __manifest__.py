cebf9c0c7a7f4610d5ebff58f79ea5623c0ee1a6
CESL64 <carl.salinas64@gmail.com>
2026-04-21 10:59:21 -0600

    [ADD] Funcionalidad de Tracking
    Se agrego la funcionalidad de tracking segun cambio del campo estado de la orden de compras.

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Purchase State Change Registry",
+    "version": "18.0.1.0.0",
+    "summary": "Registro de cambios de estado para compras",
+    "description": "Hereda purchase.order para registrar cambios en state.",
+    "author": "Prueba Tecnica",
+    "license": "LGPL-3",
+    "category": "Purchases",
+    "depends": [
+        "purchase",
+        "state_change_registry",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
